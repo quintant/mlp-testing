b6f69ad9c0fec69e0a910d4ddeeb9efe903ac818
Sigurjon <sigurjoni52@gmail.com>
2024-05-16 16:18:28 +0100

    add run script

---
+++ run.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --output=SD.txt
+#SBATCH --nodes=1
+#SBATCH --mem=20G
+#SBATCH --cpus-per-task=1
+#SBATCH -p PGR-Standard
+#SBATCH --mail-type=END
+#SBATCH --mail-user=s2595230@ed.ac.uk
+
+source /home/s2595230/mlp-testing/.venv/bin/activate
+
+python SD.py
