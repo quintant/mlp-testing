a4296edf0f83d0d5279722f3ee3b83d568587b62
Sigurjon <sigurjoni52@gmail.com>
2024-06-01 19:50:56 +0100

    Generate more images

--- SD.py
+++ SD.py
@@ -1,15 +1,37 @@
-
-from diffusers import AutoPipelineForText2Image
+import PIL.Image
 import torch
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+import PIL
+import os
+import uuid
 
-pipeline = AutoPipelineForText2Image.from_pretrained(
-    "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, variant="fp16"
-).to("cuda")
-generator = torch.Generator("cuda").manual_seed(31)
-image = pipeline(
-    "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k",
-    generator=generator,
-).images[0]
+os.makedirs("output", exist_ok=True)
+MODEL_ID = "stabilityai/stable-diffusion-2-1"
+NUM_IMG = 1_000
+NUM_IMG_PER_PROMPT = 2
 
-# Save the image
-image.save("output.png")
+PROMPT = "Portrait of a person, photo, high quality, high resolution, vivid, sharp, clear, detailed, realistic"
+NEG_PROMPT = "low quality, blurry, noisy, bad quality, worse quality, poor quality, low resolution, low res, pixelated, camera, black and white, old, vintage, painting, drawing"
+
+# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
+pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16)
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe = pipe.to("cuda")
+
+
+# Generate images
+for i in range(NUM_IMG//NUM_IMG_PER_PROMPT):
+
+    images:tuple[PIL.Image.Image] = pipe(
+        prompt=PROMPT, 
+        neg_prompt=NEG_PROMPT, 
+        return_dict=False, 
+        num_images_per_prompt=NUM_IMG_PER_PROMPT
+    )[0]
+
+    print(f"Generated {len(images)} images")
+    
+    for img in images:
+        file_id = uuid.uuid4()
+        img.save(f"output/{file_id}.png")
+        
