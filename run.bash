e6f48855dee084f53e5752f71952228664ea979f
Sigurjon <sigurjoni52@gmail.com>
2024-06-04 17:50:23 +0100

    jfk

--- run.bash
+++ run.bash
@@ -13,17 +13,4 @@ source /home/s2595230/mlp-testing/.venv/bin/activate
 export MODEL_NAME="stabilityai/stable-diffusion-2-1"
 export dataset_name="./data/"
 
-accelerate launch --mixed_precision="fp16"  train_text_to_image.py \
-  --pretrained_model_name_or_path=$MODEL_NAME \
-  --dataset_name=$dataset_name \
-  --use_ema \
-  --resolution=768 --center_crop --random_flip \
-  --train_batch_size=16 \
-  --gradient_accumulation_steps=4 \
-  --gradient_checkpointing \
-  --max_train_steps=15000 \
-  --learning_rate=1e-05 \
-  --max_grad_norm=1 \
-  --enable_xformers_memory_efficient_attention
-  --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --output_dir="fine-tuned-test-model"
+accelerate launch --mixed_precision="fp16"  train_text_to_image.py --pretrained_model_name_or_path=$MODEL_NAME --dataset_name=$dataset_name --use_ema --resolution=768 --center_crop --random_flip --train_batch_size=16 --gradient_accumulation_steps=4 --gradient_checkpointing --max_train_steps=15000 --learning_rate=1e-05 --max_grad_norm=1 --enable_xformers_memory_efficient_attention--lr_scheduler="constant" --lr_warmup_steps=0 --output_dir="fine-tuned-test-model"
