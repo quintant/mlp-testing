aa71e186001ce90dda18cb17dbb582481e9d5d4c
Sigurjon Ingi <sigurjoni52@gmail.com>
2024-06-20 18:20:41 +0100

    run config

--- run.bash
+++ run.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --gres=gpu:8
+#SBATCH --nodelist=landonia06
+#SBATCH --gres=gpu:a6000:2
 #SBATCH --output=SD.txt
 #SBATCH --nodes=1
 #SBATCH --mem=20G
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=12
 #SBATCH -p PGR-Standard
 #SBATCH --mail-type=END
 #SBATCH --mail-user=s2595230@ed.ac.uk
@@ -16,6 +17,6 @@ while true; do
   sleep 10
 done &
 
-python3 custom_train.py --data_dir "./data/train" --batch_size 12 --num_workers 16 --lr 1e-5 --resolution 512 --center_crop --random_flip --dataparallel
+python3 custom_train.py --run_id "hands_only" --batch_size 32 --num_workers 12 --lr 1e-5 --resolution 768 --center_crop --random_flip --dataparallel --no_split --num_generations 100
 
 kill %1s
