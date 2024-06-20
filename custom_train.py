bf695df44472062e7d39b103789a706fde1d1dc3
Sigurjon Ingi <sigurjoni52@gmail.com>
2024-06-20 17:55:47 +0100

    gen_train loop

--- custom_train.py
+++ custom_train.py
@@ -5,6 +5,7 @@ import os
 import random
 import shutil
 from pathlib import Path
+import uuid
 
 import accelerate
 import datasets
@@ -106,6 +107,7 @@ def create_parallel_models(
         unet: UNet2DConditionModel,
         text_encoder: CLIPTextModel,
         compile: bool = False,
+        no_split: bool = True,
 
 ) -> Tuple[
     AutoencoderKL, UNet2DConditionModel, CLIPTextModel, int, int, int
@@ -114,6 +116,21 @@ def create_parallel_models(
     Create parallel models for training
     returns: Tuple[AutoencoderKL, UNet2DConditionModel, CLIPTextModel, vae_device, text_encoder_device, unet_device]
     """
+    if no_split:
+        vae_device = 0
+        text_encoder_device = 0
+        unet_device = 0
+        vae.to(f"cuda:{vae_device}")
+        text_encoder.to(f"cuda:{text_encoder_device}")
+        unet.to(f"cuda:{unet_device}")
+        if compile:
+            vae = torch.compile(vae)
+            text_encoder = torch.compile(text_encoder)
+            unet = torch.compile(unet)
+        vae = torch.nn.DataParallel(vae)
+        text_encoder = torch.nn.DataParallel(text_encoder)
+        unet = torch.nn.DataParallel(unet)
+        return vae, unet, text_encoder, vae_device, text_encoder_device, unet_device
     # Get number of devices
     num_devices = torch.cuda.device_count()
     print(f"Number of devices: {num_devices}")
@@ -149,7 +166,7 @@ def create_parallel_models(
 
     return vae, unet, text_encoder, *main_devices
 
-def get_dataset(args: argparse.Namespace) -> datasets.Dataset:
+def get_dataset(data_dir: str, args: argparse.Namespace) -> datasets.Dataset:
     print("Getting dataset")
     data_transforms = transforms.Compose(
         [
@@ -161,52 +178,67 @@ def get_dataset(args: argparse.Namespace) -> datasets.Dataset:
         ]
     )
     dataset = ArtificialImagesDataset(
-        data_dir=Path(args.data_dir),
+        data_dir=Path(data_dir),
         transform=data_transforms,
         model_name=MODEL_NAME,
     )
     return dataset
 
+def save_model(unet: UNet2DConditionModel, run_id: str, generation: int):
+    print("Saving model")
+    unet.save_pretrained(f"runs/{run_id}/models/unet_{generation}")
 
-def main(args: argparse.Namespace):
-    print("Starting training")
+def generate_training_data(
+        unet: UNet2DConditionModel,
+        vae: AutoencoderKL,
+        text_encoder: CLIPTextModel,
+        scheduler: DDPMScheduler,
+        tokenizer: CLIPTokenizer,
+        run_id: str,
+        num_images: int,
+        generation: int,
+        device: str,
+        resolution: int,
+        prompt: str,
+        no_images_per_generation: int = 16,
+    ):
+    unet.eval()
+    with torch.no_grad():
+        print("Generating training data")
+        pipe = StableDiffusionPipeline(
+            vae=vae,
+            unet=unet,
+            text_encoder=text_encoder,
+            scheduler=scheduler,
+            tokenizer=tokenizer,
+            device=device,  # Use the same device as the model
+            requires_safety_checker=False,
+        )
 
-    vae, unet, text_encoder, scheduler, _ = load_models(MODEL_NAME)
-    
-    if args.dataparallel:
-        print("Creating parallel models")
-        vae, unet, text_encoder, vae_device, text_encoder_device, unet_device = create_parallel_models(vae, unet, text_encoder, compile=args.compile)
+        os.makedirs(f"runs/{run_id}/data/{generation}", exist_ok=True)
+        metadata = []
+        for i in range(num_images // no_images_per_generation):
+            images = pipe(
+                prompt=prompt,
+                return_dict=False,
+                num_images_per_prompt=no_images_per_generation,
+                resolution=resolution,
+            )[0]
+            for img in images:
+                file_id = uuid.uuid4()
+                img.save(f"runs/{run_id}/data/{generation}/{file_id}.png")
+                metadata.append(
+                    f"{{'file_name': '{file_id}.png', 'text': '{prompt}'}}"
+                )
 
-        vae_device = f"cuda:{vae_device}"
-        text_encoder_device = f"cuda:{text_encoder_device}"
-        unet_device = f"cuda:{unet_device}"
-    else:
-        vae_device = "cuda" if torch.cuda.is_available() else "cpu"
-        text_encoder_device = "cuda" if torch.cuda.is_available() else "cpu"
-        unet_device = "cuda" if torch.cuda.is_available() else "cpu"
+    with open(f"runs/{run_id}/data/{generation}/metadata.jsonl", "w") as f:
+        for x in metadata:
+            f.write(x + "\n")
 
-    print("Creating optimizer")
-    optimizer = torch.optim.AdamW(
-        unet.parameters(),
-        lr=args.lr,
-        betas=(args.beta1, args.beta2),
-        weight_decay=args.weight_decay,
-        eps=args.adam_epsilon,
-    )
+    unet.train()
 
-    print("Getting dataset")
-    dataset = get_dataset(args)
 
-    print("Creating dataloader")
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=args.batch_size,
-        shuffle=True,
-        num_workers=args.num_workers,
-        pin_memory=True,
-        drop_last=True,
-    )
-
+def train(dataloader, vae, unet, text_encoder, scheduler, optimizer, args, vae_device, text_encoder_device, unet_device):
     # Gradient scaler
     print("Starting training loop")
     for epoch in range(args.epochs):
@@ -265,11 +297,77 @@ def main(args: argparse.Namespace):
             print("Updating progress bar")
 
 
+def main(args: argparse.Namespace):
+    print("Starting training")
+    generation = 0
+    run_id = args.run_id
+    print("Creating run directory")
+    os.makedirs(f"runs/{run_id}", exist_ok=True)
+    total_generations = args.num_generations
+    num_images = args.num_images
+
+
+    vae, unet, text_encoder, scheduler, tokenizer = load_models(MODEL_NAME)
+    
+    if args.dataparallel:
+        print("Creating parallel models")
+        vae, unet, text_encoder, vae_device, text_encoder_device, unet_device = create_parallel_models(vae, unet, text_encoder, compile=args.compile, no_split=args.no_split)
+
+        vae_device = f"cuda:{vae_device}"
+        text_encoder_device = f"cuda:{text_encoder_device}"
+        unet_device = f"cuda:{unet_device}"
+    else:
+        vae_device = "cuda" if torch.cuda.is_available() else "cpu"
+        text_encoder_device = "cuda" if torch.cuda.is_available() else "cpu"
+        unet_device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    print("Creating optimizer")
+    optimizer = torch.optim.AdamW(
+        unet.parameters(),
+        lr=args.lr,
+        betas=(args.beta1, args.beta2),
+        weight_decay=args.weight_decay,
+        eps=args.adam_epsilon,
+    )
+
+    for generation in range(total_generations):
+        generate_training_data(
+            unet,
+            vae,
+            text_encoder,
+            scheduler,
+            tokenizer,
+            run_id,
+            num_images=num_images,
+            generation=generation,
+            device=unet_device,
+            resolution=args.resolution,
+            prompt="image of hands, photo, high quality, high resolution, vivid, sharp, clear, detailed, realistic",
+        )
+
+        dataset = get_dataset(f"runs/{run_id}/data/{generation}", args)
+
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=args.num_workers,
+            pin_memory=True,
+            drop_last=False,
+        )
+
+        train(dataloader, vae, unet, text_encoder, scheduler, optimizer, args, vae_device, text_encoder_device, unet_device)
+
+        save_model(unet, run_id, generation)
+
+
+
+
 
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, required=True)
+    parser.add_argument("--run_id", type=str, required=True)
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--num_workers", type=int, default=4)
     parser.add_argument("--lr", type=float, default=1e-4)
@@ -284,6 +382,10 @@ if __name__ == "__main__":
     parser.add_argument("--compile", action="store_true")
     parser.add_argument("--dataparallel", action="store_true")
     parser.add_argument("--clip_grad_norm", type=float, default=-1)
+    parser.add_argument("--no_split", action="store_true")
+    parser.add_argument('--num_generations', type=int, default=10)
+    parser.add_argument('--num_images', type=int, default=10_000)
+
     args = parser.parse_args()
     main(args)
 
