42f6de054e45a7bcd874c83a6e14b0a3b3353c24
Sigurjon <sigurjoni52@gmail.com>
2024-06-24 19:20:01 +0100

    16 images per gpu

--- new_run.bash
+++ new_run.bash
@@ -18,6 +18,6 @@ while true; do
   sleep 10
 done &
 
-python3 run.py --run_id "hands_only" --batch_size 32 --num_workers 8 --lr 1e-5 --resolution 768 --center_crop --random_flip --dataparallel --no_split --num_generations 100 --num_images 64 --images_per_generation 32
+python3 run.py --run_id "hands_only" --batch_size 32 --num_workers 8 --lr 1e-5 --resolution 768 --center_crop --random_flip --dataparallel --no_split --num_generations 100 --num_images 64 --images_per_generation 16
 
 kill %1s
